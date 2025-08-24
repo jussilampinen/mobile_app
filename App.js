98ec4a783c602c6bc801c21723e467f7864cae1d
JLampinan <jussitussi16@gmail.com>
2025-08-24 19:40:37 +0300

    Basic +/- calculator

--- App.js
+++ App.js
@@ -1,11 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import React, { useState } from "react";
 
 export default function App() {
+  const [num1, setNum1] = useState("");
+  const [num2, setNum2] = useState("");
+  const [result, setResult] = useState(null);
+
+  const calculate = (op) => {
+    const a = Number(num1);
+    const b = Number(num2);
+    if (op === "+") {
+      setResult(a + b);
+    } else if (op === "-") {
+      setResult(a - b);
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     <View style={styles.container}>
+      <Text style={styles.title}>Laskin</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Ensimmäinen numero"
+        keyboardType="numeric"
+        value={num1}
+        onChangeText={setNum1}
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Toinen numero"
+        keyboardType="numeric"
+        value={num2}
+        onChangeText={setNum2}
+      />
+
+      <View style={styles.buttonRow}>
+        <View style={styles.button}>
+          <Button title="+" onPress={() => calculate("+")} />
+        </View>
+        <View style={styles.button}>
+          <Button title="-" onPress={() => calculate("-")} />
+        </View>
+      </View>
+
+      {result !== null && <Text style={styles.result}>Tulos: {result}</Text>}
     </View>
   );
 }
